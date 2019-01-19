ce25bc17d07c010f8b5ae9904cd55b24aa5dd7a5
foryourselfand <foryourselfand@gmail.com>
2019-01-19 21:56:09 +0300

    fingers with right scale on both sides

--- Droplet Shufflet/Assets/Scripts/PlayManager.cs
+++ Droplet Shufflet/Assets/Scripts/PlayManager.cs
@@ -4,36 +4,54 @@ public class PlayManager : MonoBehaviour
 {
     public GameObject ShadowParent;
     public GameObject BoysParent;
+    public GameObject FingersParent;
 
     private GameObject[] _shadows;
     private GameObject[] _boys;
+    private GameObject[] _fingers;
 
     private void Awake()
     {
         SaveInArray(ShadowParent, ref _shadows);
         SaveInArray(BoysParent, ref _boys);
+        SaveInArray(FingersParent, ref _fingers);
     }
 
     private void Start()
     {
-        foreach (var boy in _boys)
+        ActionWhenStart();
+    }
+
+    private void ActionWhenStart()
+    {
+        for (var i = 0; i < _boys.Length; i++)
         {
             while (true)
             {
                 var tempNumber = Random.Range(0, _shadows.Length);
 
                 if (_shadows[tempNumber].transform.childCount != 1) continue;
-                boy.transform.parent = _shadows[tempNumber].transform;
-                boy.transform.localPosition = new Vector2(0, 0.12F);
+                _boys[i].transform.parent = _shadows[tempNumber].transform;
+                _boys[i].transform.localPosition = new Vector2(0, 0.12F);
+                _fingers[i].transform.parent = _shadows[tempNumber].transform;
+                _fingers[i].transform.localPosition = new Vector2(0, 0.82F);
                 break;
             }
         }
 
+        ChangeScaleOnFingers();
 
-        foreach (var glass in _shadows)
-        {
-            glass.GetComponent<PositionChanger>().SetTarget(new Vector2(glass.transform.position.x, 1));
-        }
+//        foreach (var shadow in _shadows)
+//        {
+//            shadow.GetComponent<PositionChanger>().SetTarget(new Vector2(shadow.transform.localPosition.x, 0.5F));
+//        }
+    }
+
+    private void ChangeScaleOnFingers()
+    {
+        var temp = _fingers[0].transform.position.x < _fingers[1].transform.position.x ? -1 : 1;
+        _fingers[0].transform.localScale = new Vector3(1 * temp, 1, 1);
+        _fingers[1].transform.localScale = new Vector3(-1 * temp, 1, 1);
     }
 
     private void SaveInArray(GameObject parent, ref GameObject[] objectsArray)

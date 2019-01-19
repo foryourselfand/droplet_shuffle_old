1e0c6c60913efb2e0a6b1d4d1c0e72805d756359
foryourselfand <foryourselfand@gmail.com>
2019-01-19 22:05:34 +0300

    change stucture of set parent in start

--- Droplet Shufflet/Assets/Scripts/PlayManager.cs
+++ Droplet Shufflet/Assets/Scripts/PlayManager.cs
@@ -31,27 +31,20 @@ public class PlayManager : MonoBehaviour
                 var tempNumber = Random.Range(0, _shadows.Length);
 
                 if (_shadows[tempNumber].transform.childCount != 1) continue;
-                _boys[i].transform.parent = _shadows[tempNumber].transform;
-                _boys[i].transform.localPosition = new Vector2(0, 0.12F);
-                _fingers[i].transform.parent = _shadows[tempNumber].transform;
-                _fingers[i].transform.localPosition = new Vector2(0, 0.82F);
+
+                SetParentToShadowAndSetY(_boys[i], tempNumber, 0.12F);
+                SetParentToShadowAndSetY(_fingers[i], tempNumber, 0.82F);
+
                 break;
             }
         }
 
         ChangeScaleOnFingers();
 
-//        foreach (var shadow in _shadows)
-//        {
-//            shadow.GetComponent<PositionChanger>().SetTarget(new Vector2(shadow.transform.localPosition.x, 0.5F));
-//        }
-    }
-
-    private void ChangeScaleOnFingers()
-    {
-        var temp = _fingers[0].transform.position.x < _fingers[1].transform.position.x ? -1 : 1;
-        _fingers[0].transform.localScale = new Vector3(1 * temp, 1, 1);
-        _fingers[1].transform.localScale = new Vector3(-1 * temp, 1, 1);
+        foreach (var shadow in _shadows)
+        {
+            shadow.GetComponent<PositionChanger>().SetTarget(new Vector2(shadow.transform.localPosition.x, 0.5F));
+        }
     }
 
     private void SaveInArray(GameObject parent, ref GameObject[] objectsArray)
@@ -63,5 +56,18 @@ public class PlayManager : MonoBehaviour
         {
             objectsArray[i] = parent.transform.GetChild(i).gameObject;
         }
+    }
+
+    private void SetParentToShadowAndSetY(GameObject objectToChange, int shadowNumber, float yToChange)
+    {
+        objectToChange.transform.parent = _shadows[shadowNumber].transform;
+        objectToChange.transform.localPosition = new Vector2(0, yToChange);
+    }
+
+    private void ChangeScaleOnFingers()
+    {
+        var temp = _fingers[0].transform.position.x < _fingers[1].transform.position.x ? -1 : 1;
+        _fingers[0].transform.localScale = new Vector3(1 * temp, 1, 1);
+        _fingers[1].transform.localScale = new Vector3(-1 * temp, 1, 1);
     }
 }
